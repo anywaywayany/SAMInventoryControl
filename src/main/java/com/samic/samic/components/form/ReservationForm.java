e6eb2f6e1aa3587df784930d58714704f10a1ea3
Aleksandar Zivkovic <ziv22671@spengergasse.at>
2023-12-26 03:58:53 +0100

    Frontend/FreieLagerobjekteView - View, ReservationForm implementiert
    dataImplFullDB.sql mit über 3000 Datensätzen hinzugefügt
    
    Geänderte/hinzugefügte Dateien:
    src/main/java/com/samic/samic/components/form/ReservationForm.java
    src/main/java/com/samic/samic/views/MainLayout.java
    src/main/java/com/samic/samic/views/freie_lagerobjekte/FreieLagerobjekteView.java
    src/main/java/com/samic/samic/views/lagerobjekt_erfassen/LagerobjektErfassenView.java
    src/main/resources/dataImpFullDB.sql
    
    Signed-off: Aleksandar Zivkovic (ziv2267@spengergasse.at)
    
    	modified:   src/main/java/com/samic/samic/components/form/CPEForm.java
    	new file:   src/main/java/com/samic/samic/components/form/ReservationForm.java
    	modified:   src/main/java/com/samic/samic/views/MainLayout.java
    	new file:   src/main/java/com/samic/samic/views/freie_lagerobjekte/FreieLagerobjekteView.java
    	modified:   src/main/java/com/samic/samic/views/lagerobjekt_erfassen/LagerobjektErfassenView.java
    	modified:   src/main/resources/data.sql
    	new file:   src/main/resources/dataImpFullDB.sql

---
+++ src/main/java/com/samic/samic/components/form/ReservationForm.java
@@ -0,0 +1,44 @@
+package com.samic.samic.components.form;
+
+import com.samic.samic.data.entity.Reservation;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+
+@Component
+@Scope("prototype")
+public class ReservationForm extends FormLayout {
+    private final TextArea description = new TextArea("Beschreibung");
+    private final IntegerField reservedFor = new IntegerField("Verbindungsnummer (Optional)");
+    private final BeanValidationBinder<Reservation> reservationBinder = new BeanValidationBinder<>(Reservation.class);
+
+    private Reservation reservation;
+    @PostConstruct
+    public void initUI() {
+        description.setPlaceholder("Zusätzliche relevante Informationen zu meiner Reservierung ...");
+        reservedFor.setRequired(false);
+        add(description, reservedFor);
+        initBinder();
+    }
+
+    private void initBinder() {
+        reservationBinder.forField(description).bind(Reservation::getReservedDescription, Reservation::setReservedDescription);
+        //reservationBinder.bind(reservedFor, reservation1 -> reservation.getCustomer().connectionNo(), (reservation, t) ->reservation.setCustomer(Customer.builder().connectionNo(t).build()));
+    }
+
+
+    public void setBean(Reservation reservation) {
+        this.reservation = reservation;
+        reservationBinder.setBean(this.reservation);
+    }
+
+    public Reservation save() {
+        return reservationBinder.getBean();
+    }
+}
