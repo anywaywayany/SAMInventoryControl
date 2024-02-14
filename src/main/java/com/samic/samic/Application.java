81181ba13f3a03d2382787208f39eeb5146a7809
Kuzu <kuz14544@spengergasse.at>
2024-02-14 01:31:49 +0100

    lagerobjectErfassen Methods

--- src/main/java/com/samic/samic/Application.java
+++ src/main/java/com/samic/samic/Application.java
@@ -19,33 +19,32 @@ import javax.sql.DataSource;
 
 /**
  * The entry point of the Spring Boot application.
- *
+ * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
-@NpmPackage(value = "@fontsource/inter", version = "4.5.0")
+@NpmPackage(value = "@fontsource/inter",
+            version = "4.5.0")
 @Theme(value = "samic")
 @ComponentScan(basePackages = {"com.samic.samic.data.repositories"})
 @ComponentScan(basePackages = {"com.samic.samic.services"})
 @Import(ORCLConfig.class)
 @EntityListeners(AuditingEntityListener.class)
-public class Application implements AppShellConfigurator {
+public class Application implements AppShellConfigurator{
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
 
 
     @Bean
-    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
-            SqlInitializationProperties properties, RepositoryUser repositoryUser) {
+    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource, SqlInitializationProperties properties, RepositoryUser repositoryUser){
         // This bean ensures the database is only initialized when empty
-        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
+        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties){
             @Override
-            public boolean initializeDatabase() {
-                if (repositoryUser.count() == 0L) {
+            public boolean initializeDatabase(){
+                if(repositoryUser.count() == 0L){
                     return super.initializeDatabase();
                 }
                 return false;
