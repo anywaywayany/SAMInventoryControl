279dac9adfe8312a0191832d3e0f0a1539e52661
Aleksandar Zivkovic <ziv22671@spengergasse.at>
2024-02-17 21:16:27 +0100

    fix AdminView not showing User, ObjectTypes  and Storage from Database
    Signed-off-by: Aleksandar Zivkovic <ziv22671@spengergasse.at>

--- src/main/java/com/samic/samic/views/administration/AppSettings.java
+++ src/main/java/com/samic/samic/views/administration/AppSettings.java
@@ -3,7 +3,10 @@ package com.samic.samic.views.administration;
 import com.samic.samic.components.UIFactory;
 import com.samic.samic.components.form.NotificationLimitForm;
 import com.samic.samic.components.form.StorageForm;
+import com.samic.samic.data.entity.ObjectType;
 import com.samic.samic.data.entity.Storage;
+import com.samic.samic.services.ServiceObjectType;
+import com.samic.samic.services.ServiceStorage;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
@@ -19,27 +22,33 @@ import org.vaadin.lineawesome.LineAwesomeIcon;
 @Scope("prototype")
 public class AppSettings extends VerticalLayout {
 
-  private final Grid<NotificationLimit> notificationLimitsGrid = new Grid<>(NotificationLimit.class,
+  private final Grid<ObjectType> notificationLimitsGrid = new Grid<>(ObjectType.class,
       false);
   private final Grid<Storage> storageGrid = new Grid<>(Storage.class, false);
   private final DataProviderAdmin dataProvider;
+  private final ServiceStorage storageService;
+  private final ServiceObjectType objectTypeService;
   private final NotificationLimitForm notificationLimitForm;
   private final StorageForm storageForm;
   private final IntegerField reservationMaxDuration = new IntegerField(
       "Maximale erlaubte Reservierungsdauer:");
 
-  public AppSettings(DataProviderAdmin dataProvider, NotificationLimitForm notificationLimitForm,
+  public AppSettings(DataProviderAdmin dataProvider, ServiceStorage storageService,
+      ServiceObjectType objectTypeService, ServiceObjectType objectTypeService1,
+      NotificationLimitForm notificationLimitForm,
       StorageForm storageForm) {
     this.dataProvider = dataProvider;
+    this.storageService = storageService;
+    this.objectTypeService = objectTypeService1;
     this.notificationLimitForm = notificationLimitForm;
     this.storageForm = storageForm;
 
     reservationMaxDuration.setRequired(true);
     reservationMaxDuration.setValue(dataProvider.getReservationDuration());
-    notificationLimitsGrid.setItems(dataProvider.getNotificationLimits());
-    notificationLimitsGrid.addColumn(NotificationLimit::getType);
-    notificationLimitsGrid.addColumn(item -> "Min " + item.getMinAmount().toString());
-    notificationLimitsGrid.addColumn(item -> "Max " + item.getMaxAmount().toString());
+    notificationLimitsGrid.setItems(objectTypeService.findAll().toList());
+    notificationLimitsGrid.addColumn(ObjectType::getName);
+    //TODO add min field to ObjectType
+    //notificationLimitsGrid.addColumn(item -> "Min " + item.getMinAmount().toString());
     notificationLimitsGrid.getStyle().setBorder("0px");
     notificationLimitsGrid.setMaxHeight("200px");
     //notificationLimitsGrid.setWidthFull();
@@ -50,7 +59,7 @@ public class AppSettings extends VerticalLayout {
         UIFactory.btnIconWithTooltip(LineAwesomeIcon.TRASH_SOLID.create(), "Löschen",
             e -> onRemove(item)))).setFrozenToEnd(true);
 
-    storageGrid.setItems(dataProvider.getStorages(10));
+    storageGrid.setItems(storageService.findAll().toList());
 
     storageGrid.addColumn(Storage::getName);
     storageGrid.addColumn(storage -> storage.getAddress().getStreet());
@@ -60,13 +69,12 @@ public class AppSettings extends VerticalLayout {
     storageGrid.addColumn(storage -> storage.getAddress().getCity());
     storageGrid.getStyle().setBorder("0px");
     storageGrid.setMaxHeight("300px");
-    //storageGrid.setWidthFull();
     storageGrid.addThemeVariants(GridVariant.LUMO_NO_ROW_BORDERS);
-    storageGrid.addComponentColumn(item -> new Span(
+    storageGrid.addComponentColumn(item -> !item.getName().equals("Kunde") ? new Span(
         UIFactory.btnIconWithTooltip(LineAwesomeIcon.EDIT.create(), "Bearbeiten",
             e -> onEdit(item)),
         UIFactory.btnIconWithTooltip(LineAwesomeIcon.TRASH_SOLID.create(), "Löschen",
-            e -> onRemove(item)))).setFrozenToEnd(true);
+            e -> onRemove(item))) : new Span("")).setFrozenToEnd(true);
 
 
   }
@@ -91,17 +99,17 @@ public class AppSettings extends VerticalLayout {
     );
   }
 
-  private void onRemove(NotificationLimit item) {
-    dataProvider.removeNotificationLimit(item);
+  private void onRemove(ObjectType item) {
+    objectTypeService.saveObjectTypeByObject(item);
     notificationLimitsGrid.getDataProvider().refreshAll();
   }
 
   private void onRemove(Storage storage) {
-    dataProvider.removeStorage(storage);
+    storageService.deleteByObject(storage);
     storageGrid.getDataProvider().refreshAll();
   }
 
-  private void onEdit(NotificationLimit item) {
+  private void onEdit(ObjectType item) {
     Dialog dialog = new Dialog();
     notificationLimitForm.setBean(item);
     dialog.add(
@@ -149,7 +157,7 @@ public class AppSettings extends VerticalLayout {
 
   private void onSave() {
     if (notificationLimitForm.isValid()) {
-      dataProvider.saveNotificationLimit(notificationLimitForm.saveBean());
+      objectTypeService.saveObjectTypeByObject(notificationLimitForm.saveBean());
       UIFactory.notificationSuccess("Benachrichtigungslimit gespeichert").open();
     } else {
       UIFactory.notificationError("Ändrungen können nicht übernommen werden, da Fehlerhaft").open();
@@ -159,7 +167,7 @@ public class AppSettings extends VerticalLayout {
 
   private void onSave(Storage storage) {
     if (storageForm.isValid()) {
-      dataProvider.saveStorage(storageForm.saveBean());
+      storageService.saveStorageByObject(storageForm.saveBean());
       UIFactory.notificationSuccess("Benachrichtigungslimit gespeichert").open();
     } else {
       UIFactory.notificationError("Ändrungen können nicht übernommen werden, da Fehlerhaft").open();
