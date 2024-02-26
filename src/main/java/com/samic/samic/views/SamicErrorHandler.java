e31a67576ce21c4f6ecb4d8193ade00e809be58e
Aleksandar Zivkovic <ziv22671@spengergasse.at>
2024-02-26 01:29:53 +0100

    fix Error Handler not displaying Notification, make Views use ErrorHandler
    Signed-off-by: Aleksandar Zivkovic <ziv22671@spengergasse.at>

--- src/main/java/com/samic/samic/views/SamicErrorHandler.java
+++ src/main/java/com/samic/samic/views/SamicErrorHandler.java
@@ -1,24 +1,18 @@
 package com.samic.samic.views;
 
 import com.samic.samic.components.UIFactory;
-import com.samic.samic.exceptions.SamicException;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.ErrorParameter;
-import com.vaadin.flow.router.HasErrorParameter;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.ParentLayout;
 import com.vaadin.flow.server.ErrorEvent;
 import com.vaadin.flow.server.ErrorHandler;
 import jakarta.annotation.security.PermitAll;
-import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @PermitAll
 @ParentLayout(MainLayout.class)
-public class SamicErrorHandler extends VerticalLayout implements ErrorHandler,
-    HasErrorParameter<SamicException> {
+public class SamicErrorHandler implements ErrorHandler {
 
   private static final Logger logger = LoggerFactory.getLogger(SamicErrorHandler.class);
 
@@ -26,17 +20,10 @@ public class SamicErrorHandler extends VerticalLayout implements ErrorHandler,
   public void error(ErrorEvent errorEvent) {
     logger.error("Something wrong happened", errorEvent.getThrowable());
     if (UI.getCurrent() != null) {
-      UI.getCurrent().access(() -> UIFactory.notificationError("An internal error has occurred." +
-          "Contact support for assistance."));
+      Notification notification = UIFactory.notificationError(
+          errorEvent.getThrowable().getMessage());
+      UI.getCurrent().add(notification);
+      notification.open();
     }
-  }
-
-  @Override
-  public int setErrorParameter(BeforeEnterEvent event,
-      ErrorParameter<SamicException>
-          parameter) {
-    getElement().setText(parameter.getCaughtException().getMessage());
-    logger.info("Error: {}", parameter.getException().getMessage());
-    return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
   }
 }
--- src/main/java/com/samic/samic/views/SamicServiceListener.java
+++ src/main/java/com/samic/samic/views/SamicServiceListener.java
@@ -8,22 +8,9 @@ public class SamicServiceListener implements VaadinServiceInitListener {
 
   @Override
   public void serviceInit(ServiceInitEvent event) {
-    event.addIndexHtmlRequestListener(response -> {
-      // IndexHtmlRequestListener to change the bootstrap page
-    });
+    event.getSource().addSessionInitListener(
+        initEvent -> VaadinSession.getCurrent().setErrorHandler(new SamicErrorHandler()));
 
-    event.addDependencyFilter((dependencies, filterContext) -> {
-      // DependencyFilter to add/remove/change dependencies sent to
-      // the client
-      return dependencies;
-    });
-
-    event.addRequestHandler((session, request, response) -> {
-      // RequestHandler to change how responses are handled
-      return false;
-    });
-
-    VaadinSession.getCurrent().setErrorHandler(new SamicErrorHandler());
   }
 
 }
