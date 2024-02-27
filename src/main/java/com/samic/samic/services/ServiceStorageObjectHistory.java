c06c157fe5a202b7f87454f5490fd7c3589d51ef
Kuzu <kuz14544@spengergasse.at>
2024-02-27 13:55:49 +0100

    Moving Reservation
    	- moveFromReservationToMeineHardware(Reservation reservation).

--- src/main/java/com/samic/samic/services/ServiceStorageObjectHistory.java
+++ src/main/java/com/samic/samic/services/ServiceStorageObjectHistory.java
@@ -3,8 +3,9 @@ package com.samic.samic.services;
 import com.samic.samic.data.entity.Status;
 import com.samic.samic.data.entity.StorageObject;
 import com.samic.samic.data.entity.StorageObjectHistory;
+import com.samic.samic.data.foundation.DateTimeFactory;
 import com.samic.samic.data.repositories.RepositoryStorageObjectHistory;
-import com.samic.samic.exceptions.SamicException;
+import com.samic.samic.exceptions.StorageObjectException;
 import com.samic.samic.exceptions.StorageObjectHistoryException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -25,30 +26,26 @@ public class ServiceStorageObjectHistory{
     @Autowired
     private final RepositoryStorageObjectHistory repositoryStorageObjectHistory;
 
+    public void setStorageOBjectHistory(StorageObject storageObject){
+        if(storageObject != null ){
+            //History
+            StorageObjectHistory stoH = new StorageObjectHistory();
+            stoH.setStorageObject(storageObject);
+            stoH.setInsertDateTime(DateTimeFactory.now());
+            stoH.setStatus(storageObject.getStatus());
+//                                stoH.setUntilDateTime();
+            stoH.setStorage(storageObject.getStorage());
+            stoH.setReservation(storageObject.getReservation());
+            saveStorageObjectHistoryByObject(stoH);
+        }else{
+            throw new StorageObjectException("Given StorageObject is null!");
+        }
+    }
 
     @Transactional
     public StorageObjectHistory saveStorageObjectHistoryByObject(StorageObjectHistory storageObjectHistory){
         if(storageObjectHistory != null){
-            if(storageObjectHistory.getId() != null){
-                if(doesObjectExistById(storageObjectHistory.getId())){
-                    StorageObjectHistory objectById = findStorageObjectHistoryByID(storageObjectHistory.getId());
-                    if(objectById != null){
-                        if(objectById.getId().equals(storageObjectHistory.getId())){
-                            objectById = storageObjectHistory;
-                            return repositoryStorageObjectHistory.save(objectById);
-                        }else{
-                            throw new StorageObjectHistoryException("StorageObjectHistory with id1: '%s' and id2: '%s' does not match. Some error occoured while fetch!!".formatted(objectById.getId(), storageObjectHistory.getId()));
-                        }
-                    }else{
-                        throw new StorageObjectHistoryException("StorageObjectHistory with id: '%s' does not exist in DB".formatted(storageObjectHistory.getId()));
-                    }
-                }else{
-                    throw new StorageObjectHistoryException("StorageObjectHistory with id: '%s' does not exist in DB but does have a id: ".formatted(storageObjectHistory.getId()));
-                }
-            }else{
-                StorageObjectHistory saved = repositoryStorageObjectHistory.save(storageObjectHistory);
-                return saved;
-            }
+            return repositoryStorageObjectHistory.save(storageObjectHistory);
         }else{
             throw new StorageObjectHistoryException("StorageObjectHistory is null!");
         }
@@ -59,8 +56,10 @@ public class ServiceStorageObjectHistory{
     @Transactional
     public StorageObjectHistory findStorageObjectHistoryByID(Long id){
         if(id != null){
-            if(repositoryStorageObjectHistory.findById(id).isPresent()){
-                return repositoryStorageObjectHistory.findById(id).get();
+            if(repositoryStorageObjectHistory.findById(id)
+                                             .isPresent()){
+                return repositoryStorageObjectHistory.findById(id)
+                                                     .get();
             }else{
                 throw new StorageObjectHistoryException("Could not find StorageObjectHistory with id: '%s' in DB".formatted(id));
             }
@@ -73,7 +72,8 @@ public class ServiceStorageObjectHistory{
     @Transactional
     public Optional<StorageObjectHistory> findStorageObjectHistoryByIDOptional(Long id){
         if(id != null){
-            if(repositoryStorageObjectHistory.findById(id).isPresent()){
+            if(repositoryStorageObjectHistory.findById(id)
+                                             .isPresent()){
                 return repositoryStorageObjectHistory.findById(id);
             }else{
                 throw new StorageObjectHistoryException("Could not find StorageObjectHistory with id: '%s' in DB".formatted(id));
@@ -86,7 +86,8 @@ public class ServiceStorageObjectHistory{
     //t
     public void deleteStorageObjectHistoryById(Long id){
         if(id != null){
-            if(!repositoryStorageObjectHistory.findAll().isEmpty()){
+            if(!repositoryStorageObjectHistory.findAll()
+                                              .isEmpty()){
                 repositoryStorageObjectHistory.deleteById(id);
             }else{
                 throw new StorageObjectHistoryException("StorageObjectHistory DB is empty!");
@@ -99,7 +100,8 @@ public class ServiceStorageObjectHistory{
     //t
     public void deleteByObject(StorageObjectHistory storageObjectHistory){
         if(storageObjectHistory != null){
-            if(!repositoryStorageObjectHistory.findAll().isEmpty()){
+            if(!repositoryStorageObjectHistory.findAll()
+                                              .isEmpty()){
                 repositoryStorageObjectHistory.delete(storageObjectHistory);
             }else{
                 throw new StorageObjectHistoryException("StorageObjectHistory DB is empty!");
@@ -123,7 +125,8 @@ public class ServiceStorageObjectHistory{
     @Transactional
     public Optional<StorageObjectHistory> findStorageObjectHistoryByStatusOptional(Status status){
         if(status != null){
-            if(repositoryStorageObjectHistory.findStorageObjectHistoryByStatus(status).isPresent()){
+            if(repositoryStorageObjectHistory.findStorageObjectHistoryByStatus(status)
+                                             .isPresent()){
                 return repositoryStorageObjectHistory.findStorageObjectHistoryByStatus(status);
             }else{
                 throw new StorageObjectHistoryException("Could not find StorageObjectHistory with status: '%s' in DB".formatted(status));
@@ -136,7 +139,9 @@ public class ServiceStorageObjectHistory{
     @Transactional
     public Stream<StorageObjectHistory> findALLStorageObjectHistoryByStatusOptional(Status status){
         if(status != null){
-            if(repositoryStorageObjectHistory.findStorageObjectHistoriesByStatus(status).findAny().isPresent()){
+            if(repositoryStorageObjectHistory.findStorageObjectHistoriesByStatus(status)
+                                             .findAny()
+                                             .isPresent()){
                 return repositoryStorageObjectHistory.findStorageObjectHistoriesByStatus(status);
             }else{
                 throw new StorageObjectHistoryException("Could not find StorageObjectHistories with status: '%s' in DB".formatted(status));
@@ -149,7 +154,8 @@ public class ServiceStorageObjectHistory{
     @Transactional
     public Optional<StorageObjectHistory> findStorageObjectHistoryByStorageObjectOptional(StorageObject storageObject){
         if(storageObject != null){
-            if(repositoryStorageObjectHistory.findStorageObjectHistoryByStorageObject(storageObject).isPresent()){
+            if(repositoryStorageObjectHistory.findStorageObjectHistoryByStorageObject(storageObject)
+                                             .isPresent()){
                 return repositoryStorageObjectHistory.findStorageObjectHistoryByStorageObject(storageObject);
             }else{
                 throw new StorageObjectHistoryException("Could not find StorageObjectHistories with storageObject: '%s' in DB".formatted(storageObject));
@@ -162,7 +168,9 @@ public class ServiceStorageObjectHistory{
     @Transactional
     public Stream<StorageObjectHistory> findALLStorageObjectHistoriesByLocalDateTimeOptional(LocalDateTime localDateTime){
         if(localDateTime != null){
-            if(repositoryStorageObjectHistory.findStorageObjectHistoriesByInsertDateTime(localDateTime).findAny().isPresent()){
+            if(repositoryStorageObjectHistory.findStorageObjectHistoriesByInsertDateTime(localDateTime)
+                                             .findAny()
+                                             .isPresent()){
                 return repositoryStorageObjectHistory.findStorageObjectHistoriesByInsertDateTime(localDateTime);
             }else{
                 throw new StorageObjectHistoryException("Could not find StorageObjectHistories with Date: '%s' in DB".formatted(localDateTime));
@@ -175,7 +183,9 @@ public class ServiceStorageObjectHistory{
     @Transactional
     public Stream<StorageObjectHistory> findALLStorageObjectsHistoriesByLocalDateTimeAFTEROptional(LocalDateTime localDateTime){
         if(localDateTime != null){
-            if(repositoryStorageObjectHistory.findStorageObjectHistoryByInsertDateTimeAfter(localDateTime).findAny().isPresent()){
+            if(repositoryStorageObjectHistory.findStorageObjectHistoryByInsertDateTimeAfter(localDateTime)
+                                             .findAny()
+                                             .isPresent()){
                 return repositoryStorageObjectHistory.findStorageObjectHistoryByInsertDateTimeAfter(localDateTime);
             }else{
                 throw new StorageObjectHistoryException("Could not find StorageObjectHistories with Date: '%s' in DB".formatted(localDateTime));
@@ -186,10 +196,10 @@ public class ServiceStorageObjectHistory{
     }
 
 
-
     @Transactional
     public Stream<StorageObjectHistory> findAll(){
-            return repositoryStorageObjectHistory.findAll().stream();
+        return repositoryStorageObjectHistory.findAll()
+                                             .stream();
     }
 }
 
