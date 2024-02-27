cf4ce86afc2cff21d26b1e1feda3564ea1910385
Aleksandar Zivkovic <ziv22671@spengergasse.at>
2024-02-27 12:21:35 +0100

    fix ReservationForm throwing Exception when Customer is null
    Signed-off-by: Aleksandar Zivkovic <ziv22671@spengergasse.at>

--- src/main/java/com/samic/samic/components/form/ReservationForm.java
+++ src/main/java/com/samic/samic/components/form/ReservationForm.java
@@ -3,10 +3,8 @@ package com.samic.samic.components.form;
 import com.samic.samic.data.entity.Customer;
 import com.samic.samic.data.entity.Reservation;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Scope;
@@ -16,32 +14,33 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class ReservationForm extends FormLayout {
-    private final TextArea            description       = new TextArea("Beschreibung");
-    private final TextField           reservedFor       = new TextField("Verbindungsnummer (Optional)");
-    private final Binder<Reservation> reservationBinder = new Binder<>(Reservation.class);
 
-    private Reservation reservation;
-    @PostConstruct
-    public void initUI() {
-        description.setPlaceholder("Zusätzliche relevante Informationen zu meiner Reservierung ...");
-        reservedFor.setRequired(false);
-        add(description, reservedFor);
-        initBinder();
-    }
+  private final TextArea description = new TextArea("Beschreibung");
+  private final TextField reservedFor = new TextField("Verbindungsnummer (Optional)");
+  private final Binder<Reservation> reservationBinder = new Binder<>(Reservation.class);
 
-    private void initBinder() {
-        reservationBinder.forField(description).bind(Reservation::getReservedDescription, Reservation::setReservedDescription);
-        reservationBinder.forField(reservedFor).bind(item -> item != null ? item.getCustomer().connectionNo() : "" , (item, value) -> item.setCustomer(
-            Customer.builder().connectionNo(value).build()));
-    }
+  @PostConstruct
+  public void initUI() {
+    description.setPlaceholder("Zusätzliche relevante Informationen zu meiner Reservierung ...");
+    reservedFor.setRequired(false);
+    add(description, reservedFor);
+    initBinder();
+  }
+
+  private void initBinder() {
+    reservationBinder.forField(description)
+        .bind(Reservation::getReservedDescription, Reservation::setReservedDescription);
+    reservationBinder.forField(reservedFor).bind(item -> item.getCustomer() != null ?
+        item.getCustomer().connectionNo() : "", (item, value) -> item.setCustomer(
+        Customer.builder().connectionNo(value).build()));
+  }
 
 
-    public void setBean(Reservation reservation) {
-        this.reservation = reservation;
-        reservationBinder.setBean(this.reservation);
-    }
+  public void setBean(Reservation reservation) {
+    reservationBinder.setBean(reservation);
+  }
 
-    public Reservation save() {
-        return reservationBinder.getBean();
-    }
+  public Reservation save() {
+    return reservationBinder.getBean();
+  }
 }
